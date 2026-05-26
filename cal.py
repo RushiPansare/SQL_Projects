713e7ec3ff1c753d79a3694330ce210491f68c77
RushiPansare <rushipansare38@gmail.com>
2026-05-26 17:21:16 +0530

    s

---
+++ cal.py
@@ -0,0 +1,30 @@
+def sum():
+    num1=int(input("Enter a number 1:\n"))
+    num2=int(input("Enter a number 2:\n"))
+    sum=num1+num2
+    print("sum is=",sum)
+    
+def subtraction():
+    num1=int(input("Enter a number 1:\n"))
+    num2=int(input("Enter a number 2:\n"))
+    sub=abs(num1-num2)
+    print("subtraction is=",sub)
+    
+def multiplication():
+    num1=int(input("Enter a number 1:\n"))
+    num2=int(input("Enter a number 2:\n"))
+    mult=num1*num2
+    print("multiplication is=",mult)
+    
+def division():
+    num1=int(input("Enter a number 1:\n"))
+    num2=int(input("Enter a number 2:\n"))
+    div=num1/num2
+    print("division is=",div)
+    
+    
+ 
+#sum()
+#subtraction()
+#multiplication()
+division()
